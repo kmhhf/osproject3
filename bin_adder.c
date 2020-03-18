93abd9bbf5839728b82706e6e22a955edfb8e324
kmhhf <kmhhf@umsl.edu>
2020-03-18 15:17:07 -0500

    3/18/20

--- bin_adder.c
+++ bin_adder.c
@@ -1,3 +1,5 @@
+//Adds integers using the method selectd by the master program
+
 #include <sys/sem.h>
 #include <sys/time.h>
 #include <stdio.h>
@@ -9,16 +11,17 @@
 #include <time.h>
 
 int fileSem;
-struct sembuf sem;
 
-void semLock();
-void semRelease();
+struct sembuf sem;  //struct for the semaphore
+
+void semLock();         //lock the semaphore
+void semRelease();      //release the semaphore
 
 int main(int argc, char *argv[0])
 {
     FILE *output = NULL;
 
-    key_t semFileKey = ftok("master", 1);
+    key_t semFileKey = ftok("master", 1);                   //get key for semaphore
     if (semFileKey == -1)
     {
         fprintf(stderr, "%s: Error: ", argv[0]);
@@ -26,7 +29,7 @@ int main(int argc, char *argv[0])
         exit(EXIT_FAILURE);
     }
 
-    fileSem = semget(semFileKey, 1, 0666);
+    fileSem = semget(semFileKey, 1, 0666);                  //use same semaphore as master program
     if(fileSem == -1)
     {
         fprintf(stderr, "%s: Error: ", argv[0]);
@@ -34,7 +37,7 @@ int main(int argc, char *argv[0])
         exit(EXIT_FAILURE);
     }
 
-    key_t sharedIntKey = ftok("master", 2);
+    key_t sharedIntKey = ftok("master", 2);                 //get key for shared memeory
     if(sharedIntKey == -1)
     {
         fprintf(stderr, "%s: Error: ", argv[0]);
@@ -42,7 +45,7 @@ int main(int argc, char *argv[0])
         exit(EXIT_FAILURE);
     }
     int count = atoi(argv[3]);
-    int intShmid = shmget(sharedIntKey, sizeof(int) *count, 0666);
+    int intShmid = shmget(sharedIntKey, sizeof(int) *count, 0666);  //use same shared memory as master program
     if(intShmid == -1)
     {
         fprintf(stderr, "%s: Error: ", argv[0]);
@@ -50,7 +53,7 @@ int main(int argc, char *argv[0])
         exit(EXIT_FAILURE);
     }
 
-    int* sharedInt = (int*) shmat(intShmid, NULL, 0);
+    int* sharedInt = (int*) shmat(intShmid, NULL, 0);       //connect to shared memory
     if(sharedInt == -1)
     {
         fprintf(stderr, "%s: Error: ", argv[0]);
@@ -58,64 +61,84 @@ int main(int argc, char *argv[0])
         exit(EXIT_FAILURE);
     }
 
-    if(atoi(argv[4]) == 1)
+    if(atoi(argv[4]) == 1)  //Sum using method 1
     {
         int sharedIndex = atoi(argv[1]);
         int increment = atoi(argv[2]);
         if (sharedIndex + increment < count)
         {
-            sharedInt[sharedIndex] = sharedInt[sharedIndex] + sharedInt[sharedIndex + increment];
+            sharedInt[sharedIndex] = sharedInt[sharedIndex] + sharedInt[sharedIndex + increment]; //add two ints
 
-            semLock();
+            semLock();  //lock semaphore
             time_t currentTime;
-            time(&currentTime);
+            time(&currentTime); //get time
+
+            //print time entered the critical section
             fprintf(stderr, "PID %d entered critical section at %s\n", getpid(), ctime(&currentTime));
+
+            //sleep used for debugging
 #ifdef DEBUG
             sleep(1);
 #endif
-            output = fopen("adder_log.txt", "a");
-            fprintf(output, "%d %d %d\n", getpid(), sharedIndex, 2);
-            time(&currentTime);
+
+            output = fopen("adder_log.txt", "a");                   //open log
+            fprintf(output, "%d %d %d\n", getpid(), sharedIndex, 2);//write to log
+
+            //sleep used for debugging
 #ifdef DEBUG
             sleep(1);
 #endif
+            time(&currentTime);  //get time
+
+            //print time exited the critical section
             fprintf(stderr, "PID %d exited critical section at %s\n", getpid(), ctime(&currentTime));
-            semRelease();
+            semRelease();   //release semaphore
         }
     }
 
+    //sum using method 2
     if(atoi(argv[4]) == 2)
     {
         int sharedIndex = atoi(argv[1]);
         int increment = atoi(argv[2]);
         int i;
+
+        //add the number of integers passed by the master program
         for(i = 1; i < increment && sharedIndex + i < count; i++)
         {
             sharedInt[sharedIndex] = sharedInt[sharedIndex] + sharedInt[sharedIndex + i];
             sharedInt[sharedIndex + i] = 0;
         }
 
-        semLock();
+        semLock();  //lock the semaphore
         time_t currentTime;
-        time(&currentTime);
+        time(&currentTime);  //get current time
+
+        //print time entered the critical section
         fprintf(stderr, "PID %d entered critical section at %s\n", getpid(), ctime(&currentTime));
 
+        //sleep used in debugging
 #ifdef DEBUG
         sleep(1);
 #endif
-        output = fopen("adder_log.txt", "a");
-        fprintf(output, "%d %d %d\n", getpid(), sharedIndex, increment);
+        output = fopen("adder_log.txt", "a"); //open log file
+        fprintf(output, "%d %d %d\n", getpid(), sharedIndex, increment);  //print to log file
+
+        //sleep used for debugging
+#ifdef DEBUG
+        sleep(1);
+#endif
         time(&currentTime);
-#ifdef DEBUG
-        sleep(1);
-#endif
+
+        //print time exited critical section
         fprintf(stderr, "PID %d exited critical section at %s\n", getpid(), ctime(&currentTime));
-        semRelease();
+        semRelease();   //release semaphore
     }
 
     return 0;
 }
 
+//lock semaphore
 void semLock()
 {
     sem.sem_num = 0;
@@ -124,6 +147,7 @@ void semLock()
     semop(fileSem, &sem, 1);
 }
 
+//release semaphore
 void semRelease()
 {
     sem.sem_num = 0;
@@ -131,3 +155,4 @@ void semRelease()
     sem.sem_flg = 0;
     semop(fileSem, &sem, 1);
 }
+
--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+//Kyle Moore
+//CPMSCI 4760 Project 3
+//This program uses 2 different methods to add integers from a file.
+//It uses semaphores to protect the log file.
+
+
 #include <sys/sem.h>
 #include <sys/time.h>
 #include <stdio.h>
@@ -20,11 +26,33 @@ int* sharedInt;
 int count = 0;
 char temp[40];
 
+//struture for the semaphores
 struct sembuf sem;
 
-void semLock();
-void semRelease();
-void method1(int totalChildren1, char* name, int increment);
+
+void semLock();         //function to lock semaphore
+void semRelease();      //function to release the semaphore
+void method1(int totalChildren1, char* name, int increment);    //function to add the integers using method 1
+
+//interrupt hander for the alarm
+void timer_handler(int signum)
+{
+    fprintf(stderr, "Error: Program timed out.\n");
+    shmdt(sharedInt);
+    shmctl(intShmid, IPC_RMID, NULL);
+    semctl(fileSem, 0, IPC_RMID);
+    kill(0, SIGKILL);
+}
+
+//interrupt handler for ctrl C
+void ctrlc_handler(int signum)
+{
+    fprintf(stderr, "\n^C interrupt received.\n");
+    shmdt(sharedInt);
+    shmctl(intShmid, IPC_RMID, NULL);
+    semctl(fileSem, 0, IPC_RMID);
+    kill(0, SIGKILL);
+}
 
 int main(int argc, char *argv[0])
 {
@@ -34,7 +62,11 @@ int main(int argc, char *argv[0])
     FILE *numbers = NULL;
     FILE *output = NULL;
 
+    //signal calls for the interrupt handlers
+    signal(SIGALRM, timer_handler);
+    signal(SIGINT, ctrlc_handler);
 
+    //getopt to handle any options passed to the program
     while ((opt = getopt(argc, argv, "h")) != -1)
     {
         switch(opt)
@@ -48,13 +80,17 @@ int main(int argc, char *argv[0])
         }
     }
 
+    //allows user to select the integer file
     if (argv[1] != NULL)
     {
         strncpy(numberFile, argv[1], 255);
     }
 
-    key_t semFileKey = ftok("master", 1);
-    fileSem = semget(semFileKey, 1, IPC_CREAT | 0666);
+    alarm(100);     //sets an alarm for 100 seconds.
+
+
+    key_t semFileKey = ftok("master", 1);                   //get a key for semaphore
+    fileSem = semget(semFileKey, 1, IPC_CREAT | 0666);      //creates the shared semaphore
     if (fileSem == -1)
     {
         fprintf(stderr, "%s: Error: ", argv[0]);
@@ -62,9 +98,9 @@ int main(int argc, char *argv[0])
         exit(EXIT_FAILURE);
     }
 
-    semctl(fileSem, 0, SETVAL, 1);
+    semctl(fileSem, 0, SETVAL, 1);          //set the value of semaphore to 1
 
-    numbers = fopen(numberFile, "r");
+    numbers = fopen(numberFile, "r");       //opens file to read integers
     if(numbers == NULL)
     {
         fprintf(stderr, "%s: Error: ", argv[0]);
@@ -73,7 +109,7 @@ int main(int argc, char *argv[0])
         exit(EXIT_FAILURE);
     }
 
-    output = fopen("adder_log.txt", "w");
+    output = fopen("adder_log.txt", "w");   //opens log file
     if(output == NULL)
     {
         fprintf(stderr, "%s: Error: ", argv[0]);
@@ -81,14 +117,15 @@ int main(int argc, char *argv[0])
         semctl(fileSem, 0, IPC_RMID);
         exit(EXIT_FAILURE);
     }
-    fclose(output);
+    fclose(output); //closes log file
 
+    //counts the number of integers in the file
     while(fgets(temp, 40, numbers) != NULL)
     {
         count = count + 1;
     }
 
-    key_t sharedIntKey = ftok("master", 2);
+    key_t sharedIntKey = ftok("master", 2);         //gets a key for shared memory
     if(sharedIntKey == -1)
     {
         fprintf(stderr, "%s: Error: ", argv[0]);
@@ -97,7 +134,7 @@ int main(int argc, char *argv[0])
         exit(EXIT_FAILURE);
     }
 
-    intShmid = shmget(sharedIntKey, sizeof(int) * count, 0666|IPC_CREAT);
+    intShmid = shmget(sharedIntKey, sizeof(int) * count, 0666|IPC_CREAT);   //creates shared memory
     if(intShmid == -1)
     {
         fprintf(stderr, "%s: Errror: ", argv[0]);
@@ -106,7 +143,7 @@ int main(int argc, char *argv[0])
         exit(EXIT_FAILURE);
     }
 
-    sharedInt = (int*) shmat(intShmid, NULL, 0);
+    sharedInt = (int*) shmat(intShmid, NULL, 0);            //attaches to shared memory
     if(sharedInt == -1)
     {
         fprintf(stderr, "%s: Error: ", argv[0]);
@@ -115,23 +152,26 @@ int main(int argc, char *argv[0])
         exit(EXIT_FAILURE);
     }
 
-    rewind(numbers);
+    rewind(numbers);     //goes back to the start of the file
     int i = 0;
     int* original[count];
+
+    //puts all the ints from the file into shared memory
     for(i = 0; i < count; i++)
     {
         fgets(temp, 40, numbers);
         sharedInt[i] = atoi(temp);
         original[i] = sharedInt[i];
     }
-    fclose(numbers);
+    fclose(numbers); //closes the file
     numbers = NULL;
 
 
-    totalChildren1 = count / 2;
-    method1(totalChildren1, argv[0], 1);
-    printf("%d\n", sharedInt[0]);
+    totalChildren1 = count / 2;             //calculate how many groups for method 1.
+    method1(totalChildren1, argv[0], 1);    //sum integers using method 1
+    printf("The total for method 1 was %d\n", sharedInt[0]); //print results
 
+    //put the original ints back in shared memory
     for(i = 0; i < count; i++)
     {
         sharedInt[i] = original[i];
@@ -139,16 +179,15 @@ int main(int argc, char *argv[0])
 
     int groups;
 
-    increment = ceil(log(count));
-    groups = ceil(count/ceil(log(count)));
-    printf("%d\n", groups);
+    increment = ceil(log(count));           //calculate how many numbers to be summed per group
+    groups = ceil(count/ceil(log(count)));  //calculate group size for method 2
 
-
+    //sum integers using method 2
     while(numChildren1 < groups)
     {
-        if(activeChildren < 19)
+        if(activeChildren < 19)         //keep chldren under 20
         {
-            pid_t processPid = fork();
+            pid_t processPid = fork();  //create new child
 
             if(processPid == -1)
             {
@@ -160,6 +199,7 @@ int main(int argc, char *argv[0])
                 exit(EXIT_FAILURE);
             }
 
+            //exec to bin_adder program
             if(processPid == 0)
             {
                 char passIndex[12];
@@ -171,11 +211,11 @@ int main(int argc, char *argv[0])
                 execl("./bin_adder", "bin_adder", passIndex, passIncrement, passCount, "2", NULL);
                 fprintf(stderr, "%s: Error: execl failed.", argv[0]);
             }
-            numChildren1++;
+            numChildren1++;         //count children
             activeChildren++;
-            startIndex = startIndex + increment;
+            startIndex = startIndex + increment;    //calculate next index
         }
-        for(i = 0; i < activeChildren; i++)
+        for(i = 0; i < activeChildren; i++)     //check for finished children
         {
             childPid = waitpid(-1, NULL, WNOHANG);
             if(childPid > 0)
@@ -185,14 +225,16 @@ int main(int argc, char *argv[0])
         }
 
     }
+    //wait for any remaining children
     for(i = 0; i < activeChildren; i++)
     {
         childPid = wait();
     }
 
-    method1(groups, argv[0], increment);
-    printf("%d\n", sharedInt[0]);
+    method1(groups, argv[0], increment); //sum the results of method 2 using method 1
+    printf("The total for method 2 was %d\n", sharedInt[0]);  //print results
 
+    //remove shared memory and semaphore
     shmdt(sharedInt);
     shmctl(intShmid, IPC_RMID, NULL);
     semctl(fileSem, 0, IPC_RMID);
@@ -200,6 +242,7 @@ int main(int argc, char *argv[0])
 
 }
 
+//lock semaphore
 void semLock()
 {
     sem.sem_num = 0;
@@ -208,6 +251,7 @@ void semLock()
     semop(fileSem, &sem, 1);
 }
 
+//release semaphore
 void semRelease()
 {
     sem.sem_num = 0;
@@ -216,6 +260,7 @@ void semRelease()
     semop(fileSem, &sem, 1);
 }
 
+//Method 1 for summing integers
 void method1(int totalChildren1, char* name, int increment)
 {
     int numChildren1 = 0;
@@ -223,14 +268,14 @@ void method1(int totalChildren1, char* name, int increment)
     int childPid;
     int activeChildren = 0;
     int i;
-    while(totalChildren1 > 0)
+    while(totalChildren1 > 0)       //keeps running while more children are needed
     {
         startIndex = 0;
         while(numChildren1 < totalChildren1)
         {
-            if(activeChildren < 19)
+            if(activeChildren < 19) //keeps children unter 20
             {
-                pid_t processPid = fork();
+                pid_t processPid = fork();  //new child
 
                 if(processPid == -1)
                 {
@@ -242,6 +287,7 @@ void method1(int totalChildren1, char* name, int increment)
                     exit(EXIT_FAILURE);
                 }
 
+                //execs to the bin_adder program
                 if(processPid == 0)
                 {
                     char passIndex[12];
@@ -254,12 +300,13 @@ void method1(int totalChildren1, char* name, int increment)
                     fprintf(stderr, "%s: Error: execl failed.", name);
                 }
 
-                numChildren1++;
+                numChildren1++;         //keeps track of children
                 activeChildren++;
-                startIndex = startIndex + increment + increment;
+                startIndex = startIndex + increment + increment;  //caclulates next index
 
             }
 
+            //checks for finished children
             for(i = 0; i < activeChildren; i++)
             {
                 childPid = waitpid(-1, NULL, WNOHANG);
@@ -270,6 +317,7 @@ void method1(int totalChildren1, char* name, int increment)
             }
 
         }
+        //waits for the rest of the children
         for(i = 0; i < activeChildren; i++)
         {
             childPid = wait();
